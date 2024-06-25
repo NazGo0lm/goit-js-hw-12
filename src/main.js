4968bacf90b051683f2df65076ae8e32cd912dd6
Nazar Nazarko <starmok20@gmail.com>
2024-06-25 17:09:31 +0300

    add file

--- src/main.js
+++ src/main.js
@@ -9,13 +9,13 @@ import { renderImages } from "./js/render-functions";
 import { getImages } from "./js/pixabay-api";
 
 const refs = {
-    imageSearchForm: document.querySelector('.search-form'),
-    imageSearchInput: document.querySelector('.search-input'),
-    submitButton: document.querySelector('.search-btn'),
-    imageList: document.querySelector('.images-list'),
-    loader: document.querySelector('.loader'),
-    loading: document.querySelector('.loading'),
-    loadBtn: document.querySelector('.load-more-btn'),
+  imageSearchForm: document.querySelector('.search-form'),
+  imageSearchInput: document.querySelector('.search-input'),
+  submitButton: document.querySelector('.search-btn'),
+  imageList: document.querySelector('.images-list'),
+  loader: document.querySelector('.loader'),
+  loading: document.querySelector('.loading'),
+  loadBtn: document.querySelector('.load-more-btn'),
 }
 
 refs.loader.style.display = 'none';
@@ -26,75 +26,74 @@ const perPage = 15;
 let queryGlobal;
 
 refs.imageSearchForm.addEventListener('submit', async e => {
-    e.preventDefault();
-    let query = e.target.elements.userData.value.trim();
-    queryGlobal = query;
-    if (query === '') {
-        refs.imageList.innerHTML = '';
-        refs.loader.style.display = 'none';
-        refs.loadBtn.style.display = 'none';
-        return iziToast.error({
-            message: 'Sorry, there are no images matching your search query. Please try again!',
-            position: 'bottomRight',
-            transitionIn: 'bounceInDown',
-            transitionOut: 'fadeOutDown',
-        });
-    }
-    refs.loader.style.display = 'block';
-    currentPage = 1;
-
-    try {
-        
-        const data = await getImages(query, currentPage);
-        //console.log(data.total)
-        maxPage = Math.ceil(data.total / perPage);
-        //console.log(maxPage)
-
-
-        
-
-        if (maxPage === 0) {
-            refs.imageList.innerHTML = '';
-            refs.loader.style.display = 'none';
-            refs.loadBtn.style.display = 'none';
-            return iziToast.error({
-            message: 'Sorry, there are no images matching your search query. Please try again!',
-            position: 'bottomRight',
-            transitionIn: 'bounceInDown',
-                transitionOut: 'fadeOutDown',
-            
-        });
-        }
-        
-        const markup = renderImages(data);
-         refs.imageList.innerHTML = markup;
-
-        const lightbox = new SimpleLightbox('.images-list-item a', {
-            captions: true,
-            captionSelector: 'img',
-            captionType: 'attr',
-            captionsData: 'alt',
-            captionPosition: 'bottom',
-            captionDelay: 250,
-            animationSpeed: 300,
-            widthRatio: 1,
-            heightRatio: 0.95,
-            disableRightClick: true,
-        });
-        lightbox.refresh();
-    } catch (error) {
-        // Обробка помилок, якщо потрібно
-    }
-    e.target.reset();
+  e.preventDefault();
+  let query = e.target.elements.userData.value.trim();
+  queryGlobal = query;
+  if (query === '') {
+    refs.imageList.innerHTML = '';
     refs.loader.style.display = 'none';
-    //noLoadBtn()
-    
-    refs.loadBtn.style.display = 'block';
-    
-            noLoadBtn()
-            //console.log(currentPage)
-            //console.log(maxPage)
-        
+    refs.loadBtn.style.display = 'none';
+    return iziToast.error({
+      message: 'Sorry, there are no images matching your search query. Please try again!',
+      position: 'bottomRight',
+      transitionIn: 'bounceInDown',
+      transitionOut: 'fadeOutDown',
+    });
+  }
+  refs.loader.style.display = 'block';
+  currentPage = 1;
+
+  try {
+
+    const data = await getImages(query, currentPage);
+    //console.log(data.total)
+    maxPage = Math.ceil(data.total / perPage);
+    //console.log(maxPage)
+
+
+
+
+    if (maxPage === 0) {
+      refs.imageList.innerHTML = '';
+      refs.loader.style.display = 'none';
+      refs.loadBtn.style.display = 'none';
+      return iziToast.error({
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+        position: 'bottomRight',
+        transitionIn: 'bounceInDown',
+        transitionOut: 'fadeOutDown',
+
+      });
+    }
+
+    const markup = renderImages(data);
+    refs.imageList.innerHTML = markup;
+
+    const lightbox = new SimpleLightbox('.images-list-item a', {
+      captions: true,
+      captionSelector: 'img',
+      captionType: 'attr',
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+      animationSpeed: 300,
+      widthRatio: 1,
+      heightRatio: 0.95,
+      disableRightClick: true,
+    });
+    lightbox.refresh();
+  } catch (error) {
+    // Обробка помилок, якщо потрібно
+  }
+  e.target.reset();
+  refs.loader.style.display = 'none';
+  //noLoadBtn()
+
+  refs.loadBtn.style.display = 'block';
+
+  noLoadBtn()
+  //console.log(currentPage)
+  //console.log(maxPage)
 
 
 
@@ -102,51 +101,52 @@ refs.imageSearchForm.addEventListener('submit', async e => {
 
 
 
-    
+
+
 })
 
 refs.loadBtn.addEventListener('click', async (e) => {
-    e.preventDefault();
-    //let query = e.target.elements.userData.value.trim();
-    refs.loading.style.display = 'block';
-    refs.loadBtn.style.display = 'none';
-    currentPage++;
+  e.preventDefault();
+  //let query = e.target.elements.userData.value.trim();
+  refs.loading.style.display = 'block';
+  refs.loadBtn.style.display = 'none';
+  currentPage++;
 
-    try {
-        const data = await getImages(queryGlobal, currentPage); 
-        //console.log(data);
-        
-        const markup = renderImages(data);
-        refs.imageList.insertAdjacentHTML('beforeend', markup);
+  try {
+    const data = await getImages(queryGlobal, currentPage);
+    //console.log(data);
 
-        const lightbox = new SimpleLightbox('.images-list-item a', {
-            captions: true,
-            captionSelector: 'img',
-            captionType: 'attr',
-            captionsData: 'alt',
-            captionPosition: 'bottom',
-            captionDelay: 250,
-            animationSpeed: 300,
-            widthRatio: 1,
-            heightRatio: 0.95,
-            disableRightClick: true,
-        });
-        lightbox.refresh();
-        skipOldElement()
-    } catch (error) {
-        console.log('Error: ', error);
-    }
+    const markup = renderImages(data);
+    refs.imageList.insertAdjacentHTML('beforeend', markup);
 
-    refs.loading.style.display = 'none';
-    //refs.loadBtn.style.display = 'block';
+    const lightbox = new SimpleLightbox('.images-list-item a', {
+      captions: true,
+      captionSelector: 'img',
+      captionType: 'attr',
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+      animationSpeed: 300,
+      widthRatio: 1,
+      heightRatio: 0.95,
+      disableRightClick: true,
+    });
+    lightbox.refresh();
+    skipOldElement()
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+
+  refs.loading.style.display = 'none';
+  //refs.loadBtn.style.display = 'block';
 
 
-  
-            noLoadBtn()
-            //console.log(currentPage)
-            //console.log(maxPage)
-        
-     
+
+  noLoadBtn()
+  //console.log(currentPage)
+  //console.log(maxPage)
+
+
 });
 
 
@@ -160,8 +160,8 @@ function noLoadBtn() {
         /* title: 'The End!', */
         message: "We're sorry, but you've reached the end of search results.",
       });
-    } 
-  } 
+    }
+  }
   else {
     refs.loadBtn.style.display = 'block';
   }
@@ -173,7 +173,7 @@ function skipOldElement() {
   const height = liElem.getBoundingClientRect().height;
 
   scrollBy({
-    top: height * 3,
+    top: height * 2,
     behavior: 'smooth',
   });
 }
